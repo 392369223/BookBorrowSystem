881c85c56cfe43157e17a673529f5717d4c622b8
392369223 <80324320+392369223@users.noreply.github.com>
2021-09-02 18:09:22 +0800

    Add files via upload

---
+++ srv/com/booksystem/constant/Constant.java
@@ -0,0 +1,8 @@
+package com.booksystem.constant;
+
+public class Constant {
+    public static int NOCONDITION=0;
+    public static int ONECONDITIONLIB=1;
+    public static int ONECONDITIONAUTHOR=2;
+    public static int ALLCONDITION=3;
+}
